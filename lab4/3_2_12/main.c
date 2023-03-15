0ead0c147107d7356596e81ce0e6fb467ee0ae44
Bundamm <94806624+Bundamm@users.noreply.github.com>
2023-03-15 12:47:29 +0100

    12

--- lab4/3_2_12/main.c
+++ lab4/3_2_12/main.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double* foo(int n){
+    double * w = malloc(n*sizeof(double));
+    return w;
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    double * wsk = foo(1);
+    printf("%p\n", wsk);
+
     return 0;
 }
