8c16c23ff284df3855872d826b58aa814d1dbe26
Bundamm <94806624+Bundamm@users.noreply.github.com>
2023-04-12 12:51:22 +0200

    aaaaaaaaaaaaa

--- lab8/5_2_9/main.c
+++ lab8/5_2_9/main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void wytnij(char *nap, int n, int m){
+    int dl,i;
+    for(dl = 0; nap[dl]!= 0;dl++);
+    if(dl+1 > m){
+        for(i = 0; i+m < dl; i++){
+            nap[n+i] = nap[i+m+1];
+        }
+    }
+    else{
+        if((n<dl)&&(dl+1<=m)){
+            nap[n] = 0;
+        }
+    }
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    char co[] = "Olsztyn";
+    wytnij(co,2,4);
+    printf("%s\n",co);
     return 0;
 }
