404e34ab238628341d909bbfb515a7897ad9f55d
bundamm <tomcio.szewa@gmail.com>
2023-04-15 19:10:58 +0200

    poprawka mała

--- zestaw101/Debug101/main.c
+++ zestaw101/Debug101/main.c
@@ -9,14 +9,14 @@ int foo(int*a,int*b)
 int main()
 {
     int tab[] = {5,5,-4,0,2,-3,0,-2,3,9};
-    int *wsk=tab-1;
-    int b = *(wsk+=4); //b=
-    int c = b+3; // b=  , c=
-    int d = foo(&b,&c); // b=  , c=  , d=
-    int e = (wsk+=-1)[3]; // b=  , c=  , d=  , e=
-    e = (d -= 2) + (c += 2); // b=  , c=  , d=  , e=
-    c = d + (b+=4); // b=  , c=  , d=  , e=
-    b = *wsk + e; // b=  , c=  , d=  , e=
-    b= (--c)-(d++);  // b=  , c=  , d=  , e=
+    int wsk=tab-1;
+    int b =(wsk+=4); //b=0
+    int c = b+3; // b= 0 , c= 3
+    int d = foo(&b,&c); // b= 0 , c=3  , d=-3
+    int e = (wsk+=-1)[3]; // b= 0 , c=3  , d=-3  , e=-3
+    e = (d -= 2) + (c += 2); // b= 0 , c= 5 , d= -5 , e=0
+    c = d + (b+=4); // b= 4 , c= -1 , d= -5 , e=0
+    b = *wsk + e; // b=-4  , c= -1 , d= -5 , e=0
+    b= (--c)-(d++);  // b=3  , c= -2 , d= -4 , e=0
     return 0;
 }
