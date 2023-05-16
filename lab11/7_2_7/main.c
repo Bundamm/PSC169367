55a53476aa57712288a8832d9df8c2d1b684b99a
bundamm <tomcio.szewa@gmail.com>
2023-05-16 20:35:17 +0200

    Update main.c

--- lab11/7_2_7/main.c
+++ lab11/7_2_7/main.c
@@ -22,3 +22,24 @@ int main()
 
     return 0;
 }
+
+/*
+Druga wersja bo nie jestem pewien czy o to tu chodzi³o
+struct zespolone{
+    double im;
+    double re;
+};
+
+double dodaj(struct zespolone a, struct zespolone b){
+    return (a.im + a.re) + (b.im + b.re);
+};
+
+int main()
+{
+    struct zespolone i = {7.6, 5.6};
+    struct zespolone j = {7.4, 3.6};
+    printf("%.2f\n", dodaj(i,j));
+
+    return 0;
+}
+
